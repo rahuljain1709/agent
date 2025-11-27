85b563bcc92290fbc667eb7fefb06db9c1d1bd97
rahuljain1709 <rahuljain1709@gmail.com>
2025-11-27 16:34:31 +0530

    Delete tavily.py

--- tavily.py
+++
@@ -1,7 +0,0 @@
-# To install: pip install tavily-python
-from tavily import TavilyClient
-client = TavilyClient("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")
-response = client.search(
-    query="current news in fire in hongkong"
-)
-print(response)
